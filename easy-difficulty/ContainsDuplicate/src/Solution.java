c38e88a28b518864f79460633c67708effd58471
Alberto <albibogoni@gmail.com>
2023-04-12 20:24:15 +0200

    ContainsDuplicate problem.

---
+++ easy-difficulty/ContainsDuplicate/src/Solution.java
@@ -0,0 +1,36 @@
+/*
+    Given an integer array nums, return true if any value appears at least twice in the array, and return false if every
+    element is distinct.
+*/
+
+import java.util.HashSet;
+
+public class Solution {
+
+    public boolean containsDuplicateBruteForce(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int c = 0; c < nums.length; c++) {
+                if (c != i) {
+                    if (nums[i] == nums[c])
+                        return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> support = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (!support.contains(nums[i]))
+                support.add(nums[i]);
+            else
+                return true;
+        }
+
+        return false;
+    }
+    
+}
