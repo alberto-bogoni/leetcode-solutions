d7498d22e00d0fcaa559c5ef6ecabc70f45f6e71
Alberto <albibogoni@gmail.com>
2023-04-21 07:24:07 +0200

    FourSum problem.

---
+++ medium-difficulty/FourSum/src/Solution.java
@@ -0,0 +1,53 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> quadruplets = new LinkedList<>();
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            for (int j = i + 1; j < nums.length - 2; j++) {
+                // skip same element to avoid duplicate quadruplets
+                if (j > i + 1 && nums[j] == nums[j - 1])
+                    continue;
+
+                int left = j + 1;
+                int right = nums.length - 1;
+
+                while (left < right) {
+                    int sum = nums[i] + nums[j] + nums[left] + nums[right];
+                    if ((nums[i] + nums[j] + nums[left] + nums[right]) > Integer.MAX_VALUE)
+                        return new LinkedList<>();
+
+                    if (sum == target) {
+                        quadruplets.add(List.of(nums[i], nums[j], nums[left], nums[right]));
+                        left++;
+                        right--;
+                        while (left < right && nums[left] == nums[left - 1])
+                            left++;
+                        while (left < right && nums[right] == nums[right + 1])
+                            right--;
+                    } else if (sum > target) {
+                        right--;
+                    } else {
+                        left++;
+                    }
+                }
+            }
+        }
+
+        return quadruplets;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        System.out.println(solution.fourSum(new int[]{1000000000,1000000000,1000000000,1000000000},-294967296));
+    }
+}
