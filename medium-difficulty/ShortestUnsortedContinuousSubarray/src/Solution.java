21665cc1b6888c669465cac8dbde60effcaabb32
Alberto <albibogoni@gmail.com>
2023-04-23 11:32:52 +0200

    ShortestUnsortedContinuousSubarray problem.

---
+++ medium-difficulty/ShortestUnsortedContinuousSubarray/src/Solution.java
@@ -0,0 +1,36 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Solution {
+    public int findUnsortedSubarray(int[] arr) {
+        int low = 0, high = arr.length - 1;
+
+        while (low < arr.length - 1 && arr[low] <= arr[low + 1])
+            low++;
+
+        if (low == arr.length - 1)
+            return 0;
+
+        while (high > 0 && arr[high] >= arr[high - 1])
+            high--;
+
+        int subarrayMax = Integer.MIN_VALUE, subarrayMin = Integer.MAX_VALUE;
+        for (int k = low; k <= high; k++) {
+            subarrayMax = Math.max(subarrayMax, arr[k]);
+            subarrayMin = Math.min(subarrayMin, arr[k]);
+        }
+
+        while (low > 0 && arr[low - 1] > subarrayMin)
+            low--;
+        while (high < arr.length - 1 && arr[high + 1] < subarrayMax)
+            high++;
+
+        return high - low + 1;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        System.out.println(solution.findUnsortedSubarray(new int[]{1,3,2,2,2}));
+    }
+}
