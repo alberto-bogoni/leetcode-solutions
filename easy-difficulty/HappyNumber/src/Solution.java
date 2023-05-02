7a93dd2056ba28ee3eeb81d8b3d1354e0a530974
Alberto <albibogoni@gmail.com>
2023-05-02 21:17:17 +0200

    HappyNumber problem.

---
+++ easy-difficulty/HappyNumber/src/Solution.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public boolean isHappy(int n) {
+        int slow = n, fast = n;
+        do {
+            slow = findSquareSum(slow);
+            fast = findSquareSum(findSquareSum(fast));
+        } while (slow != fast);
+
+        return slow == 1;
+    }
+
+    private static int findSquareSum(int num) {
+        int sum = 0, digit;
+        while (num > 0) {
+            digit = num % 10;
+            sum += digit * digit;
+            num /= 10;
+        }
+        return sum;
+    }
+}
