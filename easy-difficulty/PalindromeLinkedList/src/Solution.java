8fce3ee8850707c9afcd896a698eb94b6423f8a5
Alberto <albibogoni@gmail.com>
2023-05-03 21:05:01 +0200

    PalindromeLinkedList problem.

---
+++ easy-difficulty/PalindromeLinkedList/src/Solution.java
@@ -0,0 +1,36 @@
+public class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null)
+            return true;
+
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode headSecondHalf = reverse(slow);
+
+        while (head != slow) {
+            if (head.val != headSecondHalf.val) {
+               return false;
+            }
+            head = head.next;
+            headSecondHalf = headSecondHalf.next;
+        }
+
+        return true;
+    }
+
+    private static ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}
