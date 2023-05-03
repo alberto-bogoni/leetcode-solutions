9e3714a6ed6a85d22e536b94142c561b9c4871cb
Alberto <albibogoni@gmail.com>
2023-05-03 21:56:24 +0200

    ReorderList problem.

---
+++ medium-difficulty/ReorderList/src/Solution.java
@@ -0,0 +1,38 @@
+public class Solution {
+    public void reorderList(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        ListNode middle = slow;
+        ListNode headSecondHalf = reverse(middle);
+
+        ListNode headFirstHalf = head;
+        while (headFirstHalf != null && headSecondHalf != null) {
+            ListNode temp = headFirstHalf.next;
+            headFirstHalf.next = headSecondHalf;
+            headFirstHalf = temp;
+
+            temp = headSecondHalf.next;
+            headSecondHalf.next = headFirstHalf;
+            headSecondHalf = temp;
+        }
+
+        if (headFirstHalf != null)
+            headFirstHalf.next = null;
+    }
+
+    private static ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}
