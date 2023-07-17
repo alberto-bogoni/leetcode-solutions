b2776a7830e25ca375b6372c51c67ad5b3137b8c
Alberto <albibogoni@gmail.com>
2023-07-17 10:13:40 +0200

    LongestRepeatingCharacterReplacement problem.

---
+++ medium-difficulty/LongestRepeatingCharacterReplacement/src/Solution.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public int characterReplacement(String s, int k) {
+        int windowStart = 0, maxLength = 0, maxRepeatLetterCount = 0;
+        Map<Character, Integer> letterFrequencyMap = new HashMap<>();
+
+        for (int windowEnd = 0; windowEnd < s.length(); windowEnd++) {
+            char rightChar = s.charAt(windowEnd);
+            letterFrequencyMap.put(
+                    rightChar,
+                    letterFrequencyMap.getOrDefault(rightChar, 0) + 1
+            );
+
+            maxRepeatLetterCount =
+                    Math.max(maxRepeatLetterCount, letterFrequencyMap.get(rightChar));
+
+            if (windowEnd - windowStart + 1 - maxRepeatLetterCount > k) {
+                char leftChar = s.charAt(windowStart);
+                letterFrequencyMap.put(leftChar, letterFrequencyMap.get(leftChar) - 1);
+                windowStart++;
+            }
+
+            maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
+        }
+
+        return maxLength;
+    }
+}
