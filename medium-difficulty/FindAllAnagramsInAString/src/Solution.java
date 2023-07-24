8305f9cedcb6f539b1b749e70341b9901d24e3b7
Alberto <albibogoni@gmail.com>
2023-07-24 21:20:38 +0200

    FindAllAnagramsInAString problem.

---
+++ medium-difficulty/FindAllAnagramsInAString/src/Solution.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        int windowStart = 0;
+        int matched = 0;
+
+        Map<Character, Integer> charFrequencyMap = new HashMap<>();
+        for (char chr : p.toCharArray())
+            charFrequencyMap.put(chr, charFrequencyMap.getOrDefault(chr, 0) + 1);
+
+        List<Integer> resultIndices = new ArrayList<Integer>();
+        for (int windowEnd = 0; windowEnd < s.length(); windowEnd++) {
+            char rightChar = s.charAt(windowEnd);
+            if (charFrequencyMap.containsKey(rightChar)) {
+                charFrequencyMap.put(rightChar, charFrequencyMap.get(rightChar) - 1);
+                if (charFrequencyMap.get(rightChar) == 0)
+                    matched++;
+            }
+
+            if (matched == charFrequencyMap.size())
+                resultIndices.add(windowStart);
+
+            if (windowEnd >= p.length() - 1) {
+                char leftChar = s.charAt(windowStart++);
+                if (charFrequencyMap.containsKey(leftChar)) {
+                    if (charFrequencyMap.get(leftChar) == 0)
+                        matched--;
+                    charFrequencyMap.put(leftChar, charFrequencyMap.get(leftChar) + 1);
+                }
+            }
+        }
+
+        return resultIndices;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().findAnagrams("abacdacdba", "ab"));
+    }
+}
