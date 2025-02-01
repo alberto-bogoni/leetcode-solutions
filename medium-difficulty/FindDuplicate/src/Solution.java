c8cca3ea1ee476bd93fe3ba96556e6f2adf002eb
Alberto Bogoni <albibogoni@gmail.com>
2025-02-01 11:32:10 +0100

    FindDuplicate update.

--- medium-difficulty/FindDuplicate/src/Solution.java
+++ medium-difficulty/FindDuplicate/src/Solution.java
@@ -1,4 +1,7 @@
 public class Solution {
+    public static void main(String[] args) {
+        findDuplicateSecondApproach(new int[] {1,3,4,2,2});
+    }
     public static int findDuplicate(int[] nums) {
         int slow = nums[0];
         int fast = nums[0];
@@ -20,5 +23,20 @@ public class Solution {
         }
 
         return slow;
+    }
+
+    public static int findDuplicateSecondApproach(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            int correct = nums[i] - 1;
+            if (nums[i] != nums[correct]) {
+                int temp = nums[i];
+                nums[i] = nums[correct];
+                nums[correct] = temp;
+            } else
+                i++;
+        }
+
+        return nums[nums.length - 1];
     }
 }
