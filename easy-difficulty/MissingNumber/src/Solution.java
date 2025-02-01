a4c767437ae4571ee9e1a97c339fec07039e415f
Alberto Bogoni <albibogoni@gmail.com>
2025-02-01 10:17:43 +0100

    Update MissingNumber.

--- easy-difficulty/MissingNumber/src/Solution.java
+++ easy-difficulty/MissingNumber/src/Solution.java
@@ -30,4 +30,26 @@ public class Solution {
 
         return expectedSum - actualSum;
     }
+
+    public int missingNumberThirdApproach(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            if (nums[i] < nums.length && nums[i] != nums[nums[i]])
+                swap(nums, i, nums[i]);
+            else
+                i++;
+        }
+
+        for (i = 0; i < nums.length; i++)
+            if (nums[i] != i)
+                return i;
+
+        return nums.length;
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
 }
