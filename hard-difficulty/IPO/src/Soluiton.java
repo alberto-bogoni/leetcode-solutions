2cc30f04436c74e396395eb8d9a4dae0bae86cc9
Alberto <albibogoni@gmail.com>
2025-02-23 09:59:34 +0100

    IPO problem.

---
+++ hard-difficulty/IPO/src/Soluiton.java
@@ -0,0 +1,27 @@
+import java.util.PriorityQueue;
+
+class Solution {
+    public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
+        int n = profits.length;
+        PriorityQueue<Integer> minCapitalHeap =
+                new PriorityQueue<>(n, (i1, i2) -> capital[i1] - capital[i2]);
+        PriorityQueue<Integer> maxProfitHeap =
+                new PriorityQueue<>(n, (i1, i2) -> profits[i2] - profits[i1]);
+
+        for (int i = 0; i < n; i++)
+            minCapitalHeap.offer(i);
+
+        int availableCapital = w;
+        for (int i = 0; i < k; i++) {
+            while (!minCapitalHeap.isEmpty() && capital[minCapitalHeap.peek()] <= availableCapital)
+                maxProfitHeap.add(minCapitalHeap.poll());
+
+            if (maxProfitHeap.isEmpty())
+                break;
+
+            availableCapital += profits[maxProfitHeap.poll()];
+        }
+
+        return availableCapital;
+    }
+}
