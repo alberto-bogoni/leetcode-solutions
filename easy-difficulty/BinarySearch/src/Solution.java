b24cb9ab28204337240f92bb60605361bdf69265
Alberto <albibogoni@gmail.com>
2025-02-25 11:59:41 +0100

    BinarySearch problem.

---
+++ easy-difficulty/BinarySearch/src/Solution.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public int search(int[] nums, int target) {
+        return binarySearch(nums, 0, nums.length - 1, target);
+    }
+
+    public int binarySearch(int[] nums, int start, int end, int target) {
+        if (start > end)
+            return -1;
+
+        int position = (end - start) / 2;
+        if (nums[position] == target)
+            return position;
+        else if (nums[position] > nums[start])
+            binarySearch(nums, start, position, target);
+        else
+            binarySearch(nums, position, end, target);
+
+        return position;
+    }
+}
