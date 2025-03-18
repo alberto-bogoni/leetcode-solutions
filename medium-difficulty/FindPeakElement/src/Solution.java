d841803adf46a3162636ca3aae5728fc7c4eebea
Alberto Bogoni <albibogoni@gmail.com>
2025-03-18 10:12:50 +0100

    Update FindPeakElement.

--- medium-difficulty/FindPeakElement/src/Solution.java
+++ medium-difficulty/FindPeakElement/src/Solution.java
@@ -22,4 +22,15 @@ public class Solution {
             return search(nums, start, end - 1);
         }
     }
+
+    public int searchSecond(int[] nums, int start, int end) {
+        if (start >= end) return start;
+
+        int mid = start + (end - start) / 2;
+        if (nums[mid] < nums[mid + 1]) {
+            return search(nums, mid + 1, end);
+        } else {
+            return search(nums, start, end - 1);
+        }
+    }
 }
