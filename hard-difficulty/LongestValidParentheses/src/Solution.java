9573778f3c526e446bafc93621dc4c18d915e36a
Alberto Bogoni <albibogoni@gmail.com>
2025-03-20 08:19:24 +0100

    LongestValidParentheses problem.

---
+++ hard-difficulty/LongestValidParentheses/src/Solution.java
@@ -0,0 +1,24 @@
+import java.util.Stack;
+
+public class Solution {
+    public int longestValidParentheses(String s) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        int max = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+                if (stack.isEmpty()) {
+                    stack.push(i);
+                } else {
+                    max = Math.max(max, i - stack.peek());
+                }
+            }
+        }
+        return max;
+    }
+}
