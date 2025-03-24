282692e78f8625b1cbec025097c5658384712427
Alberto Bogoni <albibogoni@gmail.com>
2025-03-24 21:43:33 +0100

    LargestRectangleInHistogram problem.

---
+++ hard-difficulty/LargestRectangleInHistogram/src/Solution.java
@@ -0,0 +1,25 @@
+import java.util.Stack;
+
+public class Solution {
+    public static void main(String[] args) {
+        largestRectangleArea(new int[] {2,1,5,6,7,2,3});
+    }
+
+    public static int largestRectangleArea(int[] heights) {
+        Stack<Integer> stack = new Stack<>();
+        int maxArea = 0;
+        int n = heights.length;
+
+        for (int i = 0; i <= n; i++) {
+            int h = (i == n) ? 0 : heights[i];
+            while (!stack.isEmpty() && h < heights[stack.peek()]) {
+                int height = heights[stack.pop()];
+                int width = stack.isEmpty() ? i : i - stack.peek() - 1;
+                maxArea = Math.max(maxArea, height * width);
+            }
+            stack.push(i);
+        }
+
+        return maxArea;
+    }
+}
