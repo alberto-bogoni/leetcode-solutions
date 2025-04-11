b41672fbb45b8dd0a53c39bed8612489754b43af
Alberto Bogoni <albibogoni@gmail.com>
2025-04-11 08:39:32 +0200

    Subsets problem update.

--- medium-difficulty/Subsets/src/Solution.java
+++ medium-difficulty/Subsets/src/Solution.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Solution {
@@ -20,5 +21,23 @@ public class Solution {
         }
 
         return subsets;
+    }
+
+    public List<List<Integer>> subsetsSecond(int[] nums) {
+        List<List<Integer>> result = new LinkedList<>();
+        result.add(new ArrayList<>());
+        backtrack(nums, 0, new ArrayList<Integer>(), result);
+        return result;
+    }
+
+    public void backtrack(int[] nums, int start, List<Integer> set, List<List<Integer>> res) {
+        for (int i = start; i < nums.length; i++) {
+            set.add(nums[i]);
+            res.add(new ArrayList<>(set));
+
+            backtrack(nums, i + 1, set, res);
+
+            set.remove(set.size() - 1);
+        }
     }
 }
